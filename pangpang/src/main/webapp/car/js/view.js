4267614f02a01fbf329bc366270c87ce9b7f4056
학생 <gksruf7894@naver.com>
2023-04-07 14:26:03 +0900

    백한결커밋

--- pangpang/src/main/webapp/car/js/view.js
+++ pangpang/src/main/webapp/car/js/view.js
@@ -3,6 +3,7 @@ console.log(memberInfo.member_id)
 
 let login=memberInfo.member_id;
 
+let com='';
 record();
 function record(){
 	console.log("record열림");
@@ -11,15 +12,17 @@ function record(){
       url:"/pangpang/view",
       data:{"login":login},
       method:"get",
+      async:false,
       success:(r)=>{
          console.log('통신');
          console.log(r);
+         	com=r;
          	let html=`<tr>
                            <th width="10%"> 번호 </th>
                            <th width="10%"> 운행시작일자 </th>
                            <th width="10%"> 운행종료일자 </th>
                            <th width="10%"> 운행허가상태</th>>
-                           <th width="10%"> 비고 </th>						
+                           <th class="reason" width="10%"> 비고 </th>						
 						</tr>`
 			r.forEach((o,i)=>{
                html +=`                  
@@ -27,7 +30,7 @@ function record(){
                      <td> ${i+1}</td>
                      <td> ${o.bookcar_str_date} </td>
                      <td> ${o.bookcar_end_date} </td>
-                     <td> ${o.bookcar_yn} </td>
+                     <td><button onclick="driver(${o.bookcar_no})">${o.bookcar_yn}</button></td>
 					 <td> ${o.reason} </td>
                   </tr>`  
 						
@@ -37,3 +40,20 @@ function record(){
       
    })//ajax e	
 }//함수 e
+
+//Y일경우 운행일지페이지 작성 n일경우 사유확인 
+function driver(bookcar_no){
+	/*console.log("driver함수 열림")*/
+	com.forEach((o)=>{
+		if(o.bookcar_no==bookcar_no){
+			console.log(o.bookcar_yn)
+			if(o.bookcar_yn=='Y'){
+				alert("운행일지를 작성하세요")
+				//여기에 운행일지 경로 넣기
+				location.href="/pangpang/car/drivecar/drivecar.jsp"
+			}
+		}
+		
+	})
+
+}
